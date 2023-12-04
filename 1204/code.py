e0eb446c1c6cbb6bb796037397bf88208bafd489
Cédrine Monnet <monnetcedrine@team-lab.com>
2023-12-04 22:47:38 +0900

    refactoring

--- 1204/code.py
+++ 1204/code.py
@@ -27,22 +27,22 @@ def solve_part_1(cards):
 
 def solve_part_2(cards):
   # Track number of cards for each index, set default to 1
-  duplicate_cards = {}
+  number_of_cards = {}
+  points = {}
   for i in range(0, len(cards)):
-    duplicate_cards[i] = 1
-
-  for i in range(0, len(cards)):
+    number_of_cards[i] = 1
     total = 0
-    number_of_cards = duplicate_cards[i]
-    for n in range(0, number_of_cards):
-      for num in cards[i][1]:
-        if num in cards[i][0]:
-          total += 1
-      while total > 0:
-        duplicate_cards[i+total] += 1
-        total -= 1
-    
-  return sum(duplicate_cards.values())
+    for num in cards[i][1]:
+      if num in cards[i][0]:
+        total += 1
+    points[i] = total
+
+  for i in points.keys():
+    point = points[i]
+    for j in range(1, point+1):
+      number_of_cards[i+j] += number_of_cards[i]
+      
+  return sum(number_of_cards.values())
 
   
 cards = parse("input.txt")
