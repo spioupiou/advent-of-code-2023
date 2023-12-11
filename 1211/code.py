cf89ed7a34d8b1cba15ffd0e3ba0a505200ac3cf
Cédrine Monnet <monnetcedrine@team-lab.com>
2023-12-11 23:33:15 +0900

    day 11 part 2

--- 1211/code.py
+++ 1211/code.py
@@ -12,52 +12,52 @@ def parse(file):
   image = []
 
   i = 1
-  for line in lines:
+  for y, line in enumerate(lines):
     split_line = [*line]
-    for index, elt in enumerate(line):
+    for x, elt in enumerate(line):
       if elt == '#':
-        split_line[index] = str(i)
+        split_line[x] = Galaxy(i, (x, y))
         i += 1
     image.append(split_line)
 
   return image
 
-def expand(image):
+def expand(image, num):
   # Rows entirely made of '.'
   indexes_y = []
   for i, row in enumerate(image):
     if all(num == '.' for num in row):
       indexes_y.append(i)
 
-  # Insert new rows from bottom 
-  indexes_y.reverse()
-  for i in indexes_y:
-    image.insert(i, ['.' for _ in range(len(image[0]))])
-
   # Columns entirely made of '.'
   indexes_x = []
   for i in range(len(image[0])):
     if all(row[i] == '.' for row in image):
       indexes_x.append(i)
-      
-  # Insert new columns
-  indexes_x.reverse()
-  for i in indexes_x:
-      for row in image:
-          for num in range(0, 1_000_000):
-            row.insert(i, '.')
 
-  return image
+  expended_galaxies = []
+  for row in image:
+    for galaxy in row:
+      if type(galaxy) == Galaxy:
+        expended_galaxies.append(galaxy)
 
-def solve_part_1(space_image):
-  space_image = expand(space_image)
+  if num != 1: num -=1
 
-  all_galaxies = []
+  for galaxy in expended_galaxies:
+    original_x, original_y = galaxy.coordinates
+    for i in indexes_y:
+      if i <= original_y:
+        galaxy.coordinates = (galaxy.coordinates[0], galaxy.coordinates[1]+num)
 
-  for y in range(0, len(space_image)):
-    for x in range(0, len(space_image[y])):
-      if space_image[y][x].isdigit():
-        all_galaxies.append(Galaxy(space_image[y][x], (x, y)))
+    for j in indexes_x:
+      if j <= original_x:
+        galaxy.coordinates = (galaxy.coordinates[0]+num, galaxy.coordinates[1])
+
+  return expended_galaxies
+  
+
+def calculate_distance(space_image, num):
+  all_galaxies = expand(space_image, num)
 
   paths = []
   for i, galaxy in enumerate(all_galaxies):
@@ -66,13 +66,12 @@ def solve_part_1(space_image):
         continue
       else:
         path_length = abs(all_galaxies[j].coordinates[0] - all_galaxies[i].coordinates[0]) + abs(all_galaxies[j].coordinates[1] - all_galaxies[i].coordinates[1])
-        print(all_galaxies[i].id, all_galaxies[j].id, path_length)
         paths.append(path_length)
 
-  print(len(paths))
   return sum(paths)
 
-space = parse('input.txt')
-print(solve_part_1(space))
+space = parse('test_input.txt')
+print(calculate_distance(space, 1))
+print(calculate_distance(space, 1_000_000))
 
   
